09c36f18950b68dad747d713b6c6ea5ee0f226be
DasDarki <rageskilln@gmail.com>
2023-04-20 21:20:05 +0200

    finishing springboot target (untested)

--- targets/java/springboot.ts
+++ targets/java/springboot.ts
@@ -1,11 +1,13 @@
 import * as pathApi from "https://deno.land/std@0.182.0/path/mod.ts";
+import { encode as base64Encode } from "https://deno.land/std@0.182.0/encoding/base64.ts";
 
-import { AuthBlockNode, GlobalVariable, IPluginContext, Logger, MiddlewareNode, MiddlewareOptions, PathNode, PluginBase, RouteNode, RouterNode, AuthType, ProjectNode, AuthenticateClauseNode } from "../../library/mod.ts";
+import { AuthBlockNode, GlobalVariable, IPluginContext, Logger, MiddlewareNode, MiddlewareOptions, PluginBase, RouteNode, RouterNode, AuthType, ProjectNode, AuthenticateClauseNode, QueryParameterType } from "../../library/mod.ts";
 
 export default class JavaSpringBootTargetPlugin extends PluginBase {
 
     private readonly _middlewareHandlers: { [key: string]: (node: MiddlewareNode) => void } = {};
     private readonly _authenticatorData: {[key: string]: { params: FieldBuilder[], field: FieldBuilder, authenticateCode: string[], authorizeCode: ((clause: AuthenticateClauseNode) => string[])|undefined }} = {};
+    private readonly _createdDtos: {[key: string]: string} = {};
     private _config: TargetConfig = null!;
     private _mainPackagePath: string = null!;
     private _mainPackage: string = null!;
@@ -56,38 +58,49 @@ export default class JavaSpringBootTargetPlugin extends PluginBase {
         }
     }
 
-    private compileRouterNode(node: RouterNode, parentInterface?: ClassBuilder, parentClass?: ClassBuilder, prefixRoute?: string, prefixName?: string, preAuthenticates?: AuthenticateClauseNode[]): void {
+    private compileRouterNode(node: RouterNode, parentInterface?: ClassBuilder, parentClass?: ClassBuilder, prefixRoute?: string, prefixName?: string, preAuthenticates?: AuthenticateClauseNode[], preHeaderValidations?: MiddlewareOptions, preBodyValidations?: MiddlewareOptions): void {
         const interfaceBuilder = parentInterface || new ClassBuilder(this._controllerPackage, this.makePascalCase(node.path.name === "" ? "Main" : node.path.name) + "ControllerHandler", true);
         const authenticates = preAuthenticates || [];
+        const headerValidations = {...(preHeaderValidations || {}), ...(node.header || {})};
+        const bodyValidations = {...(preBodyValidations || {}), ...(node.body || {})};
 
         const classBuilder = parentClass || new ClassBuilder(this._controllerPackage, this.makePascalCase(node.path.name === "" ? "Main" : node.path.name) + "Controller")
             .addImport("org.springframework.beans.factory.annotation.Autowired")
             .addImport("org.springframework.web.bind.annotation.*")
+            .addImport(this._dtoPackage + ".*")
+            .addImport(this._authPackage + ".*")
             .addAnnotation("RestController")
             .addAnnotation(`RequestMapping("${node.path.path}")`)
             .addField(new FieldBuilder("handler", interfaceBuilder.name).addAnnotation("Autowired"));
+
+        interfaceBuilder
+            .addImport(this._dtoPackage + ".*")
+            .addImport(this._authPackage + ".*");
 
         if (node.authenticate && !authenticates.find(a => a === node.authenticate)) {
             authenticates.push(node.authenticate);
         }
         
         node.routers.forEach(router => {
-            this.compileRouterNode(router, interfaceBuilder, classBuilder, (prefixRoute || "") + router.path.path, (prefixName || "") + this.makePascalCase(router.path.name), authenticates);
+            this.compileRouterNode(router, interfaceBuilder, classBuilder, (prefixRoute || "") + router.path.path, (prefixName || "") + this.makePascalCase(router.path.name), authenticates, headerValidations, bodyValidations);
         });
 
         node.routes.forEach(route => {
-            this.compileRouteNode(route, interfaceBuilder, classBuilder, (prefixRoute || ""), (prefixName || ""), authenticates);
+            this.compileRouteNode(route, interfaceBuilder, classBuilder, (prefixRoute || ""), (prefixName || ""), authenticates, headerValidations, bodyValidations);
         });
 
         interfaceBuilder.save(this._controllerPackagePath);
         classBuilder.save(this._controllerPackagePath);
     }
 
-    private compileRouteNode(node: RouteNode, interfaceBuilder: ClassBuilder, classBuilder: ClassBuilder, prefixRoute: string, prefixName: string, authenticates: AuthenticateClauseNode[]): void {
-        interfaceBuilder.addMethod(new MethodBuilder(`handle${this.makePascalCase(prefixName)}${this.makePascalCase(node.path.name)}`, "Object"));
+    private compileRouteNode(node: RouteNode, interfaceBuilder: ClassBuilder, classBuilder: ClassBuilder, prefixRoute: string, prefixName: string, authenticates: AuthenticateClauseNode[], headerValidations: MiddlewareOptions, bodyValidations: MiddlewareOptions): void {
+        headerValidations = {...headerValidations, ...(node.header || {})};
+        bodyValidations = {...bodyValidations, ...(node.body || {})};
 
-        const method = new MethodBuilder(`${node.method.toLowerCase()}${this.makePascalCase(prefixName)}${this.makePascalCase(node.path.name)}`, "Object")
+        const methodName = `${this.makePascalCase(prefixName)}${this.makePascalCase(node.path.name)}`;
+        const method = new MethodBuilder(`${node.method.toLowerCase()}${methodName}`, "Object")
             .addAnnotation(`RequestMapping("${prefixRoute}${node.path.path}", method = RequestMethod.${node.method.toUpperCase()})`);
+        const interfaceMethod = new MethodBuilder(`handle${this.makePascalCase(prefixName)}${this.makePascalCase(node.path.name)}`, "Object");
 
         authenticates.forEach(authenticate => {
             const authData = this._authenticatorData[authenticate.authBlock];
@@ -105,10 +118,77 @@ export default class JavaSpringBootTargetPlugin extends PluginBase {
                 authData.authorizeCode(authenticate).forEach(code => method.addBodyLine(code));
             }
         });
+
+        const callParams: string[] = [];
+
+        for (const key in headerValidations) {
+            if (typeof headerValidations[key] === "string") {
+                const name = "header" + this.makePascalCase(this.enusreJavaAllowedName(key));
+
+                method.addParameter(new FieldBuilder(name, this.getJavaTypeOfType(headerValidations[key] as string)).addAnnotation(`RequestHeader("${key}")`));
+                interfaceMethod.addParameter(new FieldBuilder(name, this.getJavaTypeOfType(headerValidations[key] as string)));
+
+                callParams.push(name);
+            }
+        }
+
+        node.path.queryParameters.forEach(param => {
+            const name = "query" + this.makePascalCase(param.name);
+
+            method.addParameter(new FieldBuilder(name, this.getJavaTypeOfType(param.type)).addAnnotation(`RequestParam("${param.name}")`));
+            interfaceMethod.addParameter(new FieldBuilder(name, this.getJavaTypeOfType(param.type)));
+
+            callParams.push(name);
+        });
+
+        if (Object.keys(bodyValidations).length > 0) {
+            const className = this.compileBodyOptionsAsDto(bodyValidations, methodName + "Dto");
+            const name = "body";
+
+            method.addParameter(new FieldBuilder(name, className).addAnnotation("RequestBody"));
+            interfaceMethod.addParameter(new FieldBuilder(name, className));
+
+            callParams.push(name);
+        }
     
-        method.addBodyLine(`return this.handler.handle${this.makePascalCase(prefixName)}${this.makePascalCase(node.path.name)}();`);
+        method.addBodyLine(`return this.handler.handle${this.makePascalCase(prefixName)}${this.makePascalCase(node.path.name)}(${(callParams.join(", "))});`);
 
         classBuilder.addMethod(method);
+        interfaceBuilder.addMethod(interfaceMethod);
+    }
+
+    private compileBodyOptionsAsDto(bodyOptions: MiddlewareOptions, name: string): string {
+        const hash = this.generateMiddlewareOptionsHash(bodyOptions);
+        if (this._createdDtos[hash]) {
+            return this._createdDtos[hash];
+        }
+
+        const compile = (options: MiddlewareOptions, name: string, classPackage?: string) => {
+            const classBuilder = new ClassBuilder(classPackage || "", name);
+
+            for (const key in options) {
+                if (typeof options[key] === "object") {
+                    const innerClassBuilder = compile(options[key] as MiddlewareOptions, this.makePascalCase(key) + "Dto", classPackage);
+                    const className = innerClassBuilder.name;
+                    classBuilder.addClass(innerClassBuilder);
+
+                    classBuilder.addField(new FieldBuilder(this.makeCamelCase(this.enusreJavaAllowedName(key)), className));
+                    classBuilder.addMethod(new MethodBuilder(`get${this.makePascalCase(key)}`, className).addBodyLine(`return this.${this.makeCamelCase(this.enusreJavaAllowedName(key))};`));
+                } else if (typeof options[key] === "string") {
+                    classBuilder.addField(new FieldBuilder(this.makeCamelCase(this.enusreJavaAllowedName(key)), this.getJavaTypeOfType(options[key] as string)));
+                    classBuilder.addMethod(new MethodBuilder(`get${this.makePascalCase(key)}`, this.getJavaTypeOfType(options[key] as string)).addBodyLine(`return this.${this.makeCamelCase(this.enusreJavaAllowedName(key))};`));
+                }
+            }
+
+            return classBuilder;
+        };
+
+        const classBuilder = compile(bodyOptions, name, this._dtoPackage);
+        classBuilder.save(this._dtoPackagePath);
+
+        this._createdDtos[hash] = classBuilder.name;
+
+        return classBuilder.name;
     }
 
     private compileCorsMiddleware(node: MiddlewareNode): void {
@@ -160,7 +240,7 @@ export default class JavaSpringBootTargetPlugin extends PluginBase {
     }
 
     private compileAuthBlockNode(node: AuthBlockNode): void {
-        const authenticatorField = new FieldBuilder("authenticator", this.makePascalCase(node.name) + "Authenticator");
+        const authenticatorField = new FieldBuilder("authenticator", this.makePascalCase(node.name) + "Authenticator").addAnnotation("Autowired");
         const authenticateMethod = new MethodBuilder("authenticate", "boolean");
         const params: FieldBuilder[] = [];
         const authenticateCode: string[] = [];
@@ -169,9 +249,9 @@ export default class JavaSpringBootTargetPlugin extends PluginBase {
         if (node.type === AuthType.APIKey) {
             authenticateMethod.addParameter(new FieldBuilder("apiKey", "String"));
             if (node.source === "query") {
-                params.push(new FieldBuilder("apiKey", "String").addAnnotation(`RequestParam("${node.name}")`));
+                params.push(new FieldBuilder("apiKey", "String").addAnnotation(`RequestParam("${node.field}")`));
             } else if (node.source === "header") {
-                params.push(new FieldBuilder("apiKey", "String").addAnnotation(`RequestHeader("${node.name}")`));
+                params.push(new FieldBuilder("apiKey", "String").addAnnotation(`RequestHeader("${node.field}")`));
             } else {
                 Logger.fatal("body as authentication source is not supported for APIKey authentication for JavaSpringBoot target!");
                 return;
@@ -217,11 +297,43 @@ export default class JavaSpringBootTargetPlugin extends PluginBase {
         };
     }
 
-    private makePascalCase(name: string): string {
-        if (name.length === 0) {
-            return name;
+    private getJavaTypeOfType(queryType: string): string {
+        switch (queryType) {
+            case QueryParameterType.String:
+                return "String";
+            case QueryParameterType.Int:
+                return "long";
+            case QueryParameterType.Float:
+                return "double";
+            case QueryParameterType.Bool:
+                return "boolean";
+            case QueryParameterType.Date:
+                return "java.time.LocalDate";
+            case QueryParameterType.DateTime:
+                return "java.time.LocalDateTime";
+            case QueryParameterType.Time:
+                return "java.time.LocalTime";
+            default:
+                return "Object";
         }
-        return name[0].toUpperCase() + name.slice(1);
+    }
+
+    private enusreJavaAllowedName(name: string): string {
+        if (name === "default") {
+            return "default_";
+        }
+        if (name === "package") {
+            return "package_";
+        }
+
+        const trimmed = name.replace(/[^a-zA-Z0-9]/g, "");
+        if (trimmed.length === 0) {
+            return "param" + Date.now();
+        }
+        if (trimmed[0].match(/[0-9]/)) {
+            return "_" + trimmed;
+        }
+        return trimmed;
     }
 
     private makeCamelCase(name: string): string {
@@ -229,6 +341,13 @@ export default class JavaSpringBootTargetPlugin extends PluginBase {
             return name;
         }
         return name[0].toLowerCase() + name.slice(1);
+    }
+
+    private makePascalCase(name: string): string {
+        if (name.length === 0) {
+            return name;
+        }
+        return name[0].toUpperCase() + name.slice(1);
     }
 
     private initializePackagePath(base: string, packageName: string): string {
@@ -304,6 +423,27 @@ export default class JavaSpringBootTargetPlugin extends PluginBase {
             this._middlewareHandlers[node.name](node);
         }
     }
+
+    private generateMiddlewareOptionsHash(options: MiddlewareOptions): string {
+        const sortObjectKeysRecursively = (obj: {[key: string]: unknown}): {[key: string]: unknown} => {
+            const sorted: {[key: string]: unknown} = {};
+
+            Object.keys(obj).sort().forEach(key => {
+                if (typeof obj[key] === "object" && obj[key] !== null) {
+                    sorted[key] = sortObjectKeysRecursively(obj[key] as {[key: string]: unknown});
+                } else {
+                    sorted[key] = obj[key];
+                }
+            });
+
+            return sorted;
+        }
+
+        const sortedOptions = sortObjectKeysRecursively(options);
+        const trimmedOptions = JSON.stringify(sortedOptions).replace(/ /g, "").replace(/\n/g, "");
+        
+        return base64Encode(trimmedOptions);
+    }
 }
 
 class ClassBuilder {
@@ -312,6 +452,7 @@ class ClassBuilder {
     private readonly _annotations: string[] = [];
     private readonly _methods: MethodBuilder[] = [];
     private readonly _fields: FieldBuilder[] = [];
+    private readonly _innerClasses: ClassBuilder[] = [];
     private _implements: string[] = [];
 
     constructor(
@@ -330,6 +471,9 @@ class ClassBuilder {
     }
 
     public addImport(importPath: string): ClassBuilder {
+        if (this._imports.find(i => i === importPath) !== undefined) {
+            return this;
+        }
         this._imports.push(importPath);
         return this;
     }
@@ -352,6 +496,15 @@ class ClassBuilder {
         return this;
     }
 
+    public addClass(clazz: ClassBuilder): ClassBuilder {
+        if (this._isInterface) {
+            throw new Error("Cannot add inner class to interface");
+        }
+
+        this._innerClasses.push(clazz);
+        return this;
+    }
+
     public save(path: string): void {
         const filePath = pathApi.join(path, `${this._name}.java`);
 
@@ -359,7 +512,7 @@ class ClassBuilder {
     }
 
     public toString(): string {
-        let result = `package ${this._package};\n`;
+        let result = this._package === "" ? "" : `package ${this._package};\n`;
 
         if (this._imports.length > 0) {
             result += "\n";
@@ -383,14 +536,22 @@ class ClassBuilder {
         }
         result += "{\n";
 
-        this._fields.forEach(field => {
+        if (this._fields.length > 0) {
             result += "\n";
+        }
+
+        this._fields.forEach(field => {
             result += "    " + field.toString() + ";\n";
         });
 
         this._methods.forEach(method => {
             result += "\n";
             result += method.toString(this._isInterface);
+        });
+
+        this._innerClasses.forEach(clazz => {
+            result += "\n";
+            result += clazz.toString();
         });
 
         result += "\n}\n";
